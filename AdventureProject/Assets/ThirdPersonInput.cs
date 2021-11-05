028e9db9d54136e05c2bed31351cf897989a9774
GeorgiosVeropoulos <12498GR@saeinstitute.edu>
2021-11-05 16:23:19 +0200

    Movement+Camera Control

---
+++ AdventureProject/Assets/ThirdPersonInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class ThirdPersonInput : MonoBehaviour
+{
+    public FixedJoystick LeftJoyStick;
+    public FixedButton Button;
+    protected ThirdPersonUserControl Control;
+    public FixedTouchField TouchField;
+
+    protected float CameraAngle;
+    protected float CameraAngleSpeed = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Control = GetComponent<ThirdPersonUserControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Control.m_Jump = Button.Pressed;
+        Control.Hinput = LeftJoyStick.input.x;
+        Control.Vinput = LeftJoyStick.input.y;
+
+        CameraAngle += TouchField.TouchDist.x * CameraAngleSpeed;
+
+        Camera.main.transform.position = transform.position + Quaternion.AngleAxis(CameraAngle, Vector3.up) * new Vector3(0, 3, 4);
+        Camera.main.transform.rotation = Quaternion.LookRotation(transform.position + Vector3.up * 2f - Camera.main.transform.position, Vector3.up);
+    }
+}
