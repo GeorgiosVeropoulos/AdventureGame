2e924e35ed35eb87028d79180d3822bfa8d4a4c2
GeorgiosVeropoulos <12498GR@saeinstitute.edu>
2021-11-07 04:01:59 +0200

    Ground Check

--- AdventureProject/Assets/OwnThirdPersonController.cs
+++ AdventureProject/Assets/OwnThirdPersonController.cs
@@ -17,33 +17,56 @@ public class OwnThirdPersonController : MonoBehaviour
     protected float CameraAngleSpeed = 0.1f;
     protected float CameraPosY;
     protected float CameraPosSpeed = 0.1f;
+
+    public bool isGrounded;
+    
     void Start()
     {
         Rigidbody = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         anim.SetBool("Attack", false);
+        isGrounded = true;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         anim.SetBool("Attack", false);
-        var Input = new Vector3(LeftJoystick.input.x, 0, LeftJoystick.input.y);
-        var vel = Quaternion.AngleAxis(CameraAngleY + 180, Vector3.up) * Input * 4f;
+        if (Physics.Raycast(transform.position - new Vector3(0,-0.5f,0), Vector3.down, 1f))
+		{
+            
+            Debug.Log("Grounded");
+            anim.SetBool("isGround", true);
+            var Input = new Vector3(LeftJoystick.input.x, 0, LeftJoystick.input.y);
+            var vel = Quaternion.AngleAxis(CameraAngleY + 180, Vector3.up) * Input * 4f;
 
 
-        Rigidbody.velocity = new Vector3(vel.x, Rigidbody.velocity.y, vel.z);
-        transform.rotation = Quaternion.AngleAxis(CameraAngleY + 180 + Vector3.SignedAngle(Vector3.forward,
-            Input.normalized + Vector3.forward * 0.001f, Vector3.up), Vector3.up);
+            Rigidbody.velocity = new Vector3(vel.x, Rigidbody.velocity.y, vel.z);
+            transform.rotation = Quaternion.AngleAxis(CameraAngleY + 180 + Vector3.SignedAngle(Vector3.forward,
+                Input.normalized + Vector3.forward * 0.001f, Vector3.up), Vector3.up);
+
+           
+            if (Attack.Pressed == true)
+            {
+                anim.SetBool("Attack", true);
+            }
+
+            anim.SetFloat("Speed", Rigidbody.velocity.magnitude);
+        }
+		else
+		{
+            Debug.Log("AIR");
+            anim.SetBool("isGround", false);
+		}
+       
 
         CameraAngleY += Touchfield.TouchDist.x * CameraAngleSpeed;
         Camera.main.transform.position = transform.position + Quaternion.AngleAxis(CameraAngleY, Vector3.up) * new Vector3(0, 3, 4);
         Camera.main.transform.rotation = Quaternion.LookRotation(transform.position + Vector3.up * 2f - Camera.main.transform.position, Vector3.up);
-        if(Attack.Pressed == true)
-		{
-            anim.SetBool("Attack", true);
-		}
         
-        anim.SetFloat("Speed", Rigidbody.velocity.magnitude);
     }
+	
+    
 }
