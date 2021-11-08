d758cddfa534e5f75777db2e7b6d5eea2683c27b
GeorgiosVeropoulos <12498GR@saeinstitute.edu>
2021-11-09 01:33:38 +0200

    Aggro fixes (thread changes)
    Enemies now full reset if you run away from them and cant be interacted until they return to their primary position.

--- AdventureProject/Assets/EnemyController.cs
+++ AdventureProject/Assets/EnemyController.cs
@@ -6,39 +6,59 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
 
-    public bool isChasing;
+    public bool canChase;
+   
     public Transform Player;
     public Transform oldlook;
     public Transform startingPosition;
     NavMeshAgent agent;
+    Animator EnemyAnim;
+    Rigidbody rigidbody;
 
-    public float speed;
+    public float animspeed = 0; 
     public float minDist = 5;
     public float maxDist = 10;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        EnemyAnim = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         //transform.LookAt(Player);
-
+        
         if (Vector3.Distance(transform.position, Player.position) <= maxDist)
         {
-            Debug.Log("aggro");
-            if (Vector3.Distance(transform.position, Player.position) >= minDist)
-            {
-                //transform.LookAt(Player);
-                //transform.position += transform.forward * speed * Time.deltaTime;
+            //Debug.Log("aggro");
+            
+            agent.isStopped = false;
+            //animspeed = animspeed + 0.001f;
+            //EnemyAnim.SetFloat("Speed", animspeed);
+            if(canChase == true)
+			{
+                if (Vector3.Distance(transform.position, Player.position) >= minDist)
+                {
+                    //transform.LookAt(Player);
+                    //transform.position += transform.forward * speed * Time.deltaTime;
+                    agent.isStopped = false;
+                    EnemyAnim.SetFloat("Speed", 2);
+                    agent.SetDestination(Player.position);
 
-                agent.SetDestination(Player.position);
-                Debug.Log("Chasing");
-                FaceTarget();
+                    //Debug.Log("Chasing");
+                    FaceTarget();
 
+                }
+                else
+                {
+                    EnemyAnim.SetFloat("Speed", 0);
+                    agent.isStopped = true;
+                }
             }
+            
 			
         }
         else
@@ -47,9 +67,18 @@ public class EnemyController : MonoBehaviour
             
             if (Vector3.Distance(transform.position, startingPosition.position) >= 1f)
 			{
+                canChase = false;
+                EnemyAnim.SetFloat("Speed", 1.4f);
+                agent.isStopped = false;
                 agent.SetDestination(startingPosition.position);
                 
             }
+			else
+			{
+                canChase = true;
+                agent.isStopped = true;
+                EnemyAnim.SetFloat("Speed", 0);
+			}
             transform.LookAt(oldlook);
         }
 
