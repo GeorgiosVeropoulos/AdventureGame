a6ab9158f9325b5a12626dcb105a9db8077dcc0a
GeorgiosVeropoulos <12498GR@saeinstitute.edu>
2021-11-10 19:00:38 +0200

    Fixed Animations + Added more assets
    Fixed the going down a slope animation for player(no longer jumps)
    Added some testing assets

--- AdventureProject/Assets/OwnThirdPersonController.cs
+++ AdventureProject/Assets/OwnThirdPersonController.cs
@@ -44,7 +44,7 @@ public class OwnThirdPersonController : MonoBehaviour
             Camera.main.transform.position = transform.position + Quaternion.AngleAxis(CameraAngleX, Vector3.up) * new Vector3(0, 3, CameraDistance);
             Camera.main.transform.rotation = Quaternion.LookRotation(transform.position + Vector3.up * 2f - Camera.main.transform.position, Vector3.up);
             //anim.SetBool("Attack", false);
-            if (Physics.Raycast(transform.position - new Vector3(0, -0.5f, 0), Vector3.down, 0.6f))
+            if (Physics.Raycast(transform.position - new Vector3(0, -0.5f, 0), Vector3.down, 0.65f))
             {
 
                 Debug.Log("Grounded");
@@ -52,11 +52,12 @@ public class OwnThirdPersonController : MonoBehaviour
 
                 var Input = new Vector3(LeftJoystick.input.x, 0, LeftJoystick.input.y);
                 var vel = Quaternion.AngleAxis(CameraAngleX + 180, Vector3.up) * Input * 4f;
-
+                
 
                 if (stopmove == false)
                 {
-                    Rigidbody.velocity = new Vector3(vel.x, Rigidbody.velocity.y, vel.z);
+                    Rigidbody.velocity = new Vector3(vel.x, AdjustVelocityToSlope(Rigidbody.velocity).y, vel.z);
+                    
                     transform.rotation = Quaternion.AngleAxis(CameraAngleX + 180 + Vector3.SignedAngle(Vector3.forward,
                         Input.normalized + Vector3.forward * 0.001f, Vector3.up), Vector3.up);
                     anim.SetFloat("Speed", Rigidbody.velocity.magnitude);
@@ -148,4 +149,25 @@ public class OwnThirdPersonController : MonoBehaviour
         anim.SetBool("Attack", false);
         Swordscript.CanDoDamage = false;
     }
+
+
+    // Adjust velocity to go down slopes without jumping/falling
+    private Vector3 AdjustVelocityToSlope(Vector3 velocity)
+	{
+        var ray = new Ray(transform.position, Vector3.down);
+
+        if(Physics.Raycast(ray, out RaycastHit hitInfo, 0.5f))
+		{
+            var slopeRotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            var adjustedVelocity = slopeRotation * velocity;
+
+
+            if(adjustedVelocity.y < 0)
+			{
+                return adjustedVelocity;
+			}
+		}
+
+        return velocity;
+	}
 }
